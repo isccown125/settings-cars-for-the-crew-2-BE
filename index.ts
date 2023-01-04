bcbfed4d3b7ca7cfd582abc73328b7316f0d47e5
Lenovo <piwonskipiotrp>
2023-01-04 16:32:22 +0100

    installation of basic packages add index.ts add .tsconfig add .gitignore

---
+++ index.ts
@@ -0,0 +1,19 @@
+import express, {json} from "express";
+import cors from "cors";
+import 'express-async-errors';
+
+const app = express();
+
+app.use(cors({
+    origin: 'http://localhost:3000',
+}));
+app.use(json());
+
+app.get('/',  (req, res) => {
+    res.send('Test');
+})
+
+
+app.listen(3001, '0.0.0.0', () => {
+    console.log('Listening on port http://localhost:3001');
+})
