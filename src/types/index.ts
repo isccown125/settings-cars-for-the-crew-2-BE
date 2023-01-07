6f4b72047b9f8db76f2509633c458d8995e5cc49
isccown125 <92261514+isccown125@users.noreply.github.com>
2023-01-07 21:45:33 +0100

    added types
    vehicle
    settingsCar
    settingsHovercraft
    settingsMotor
    settingsPlane
    settingsBoat

---
+++ src/types/index.ts
@@ -0,0 +1,7 @@
+import * as settingsPlane from './settings-plane';
+import * as settingsMotor from './settings-motor';
+import * as settingsHovercraft from './settings-hovercraft';
+import * as settingsCar from './settings-car';
+import * as settingsBoat from './settings-boat';
+import * as vehicle from './vehicle';
+
